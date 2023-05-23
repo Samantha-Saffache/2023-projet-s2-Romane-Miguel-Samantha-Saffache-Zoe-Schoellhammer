698352eadb1dd278ef4f1607d862f01ffc329865
Samantha-Saffache <samantha.saffache@edu.univ-fcomte.fr>
2023-05-23 10:11:30 +0200

    couleurs et fonts

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,37 @@
+<script setup lang="ts">
+//a modifier avec nos icones !! //
+	import LogoIcon from '@/components/icons/IconLogo.vue'
+	import SearchIcon from '@/components/icons/IconSearch.vue'
+</script>
+
 <template>
   <header>
-    Template pour l'en-tête de page
+    <div>
+      <a href="/">
+        <LogoIcon />
+      </a>
+    </div>
+
+    <div>
+      <button>
+        <SearchIcon />
+      </button>
+
+      <button>
+        <div></div>
+        <div></div>
+        <div></div>
+      </button>
+
+			<nav>
+		    <ul>
+		      <li><a href="/mes-notifications">Mes notifications</a></li>
+		      <li><a href="/decouvrir">Découvrir</a></li>
+		      <li><a href="/mes-favoris">Mes favoris</a></li>
+		      <li><a href="/mon-compte">Mon compte</a></li>
+		      <li><a href="/messagerie">Messagerie</a></li>
+		    </ul>
+		  </nav>
+    </div>
   </header>
-  </template>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,19 +8,30 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      indigo: '#1d096b',
-      pink: '#feaa9b',
-      white: '#ffffff',
-      black: '#000000',
-      transparent: 'transparent',
-      gray: colors.zinc
-    },
-
-    fontFamily: {
-      sans: ['Mulish', 'sans-serif'],
-      serif: ['Space Mono', 'monospace']
-    },
+ "colors": {
+  "Vert": "#bcd46e",
+  "orange": "#be7015",
+  "jaune fonce": "#efdd7d",
+  "beige": "#f7f8c4",
+  "FOND": "#faebda",
+  "fond footer": "#ffffff"
+ },
+ "fontSize": {
+  "xs": "0.375rem",
+  "sm": "0.5rem",
+  "base": "0.625rem",
+  "lg": "0.6875rem",
+  "xl": "0.8125rem",
+  "2xl": "0.9375rem",
+  "3xl": "2.5rem",
+  "4xl": "6.25rem"
+ },
+ "fontFamily": {
+  "unbounded": "Unbounded",
+  "mulish": "Mulish",
+  "bebas-neue": "Bebas Neue",
+  "inter": "Inter"
+ },
 
     extend: {},
   },
