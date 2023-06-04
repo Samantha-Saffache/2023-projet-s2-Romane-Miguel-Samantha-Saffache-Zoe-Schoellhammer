a5c2d6bc4ca5a6afd9c6f9613a6757492abf4013
Samantha-Saffache <samantha.saffache@edu.univ-fcomte.fr>
2023-06-04 21:12:30 +0200

    modifications pocketbase

---
+++ PocketBase/pb_migrations/1685905881_updated_produit.js
@@ -0,0 +1,25 @@
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("2iqwzv3nrgzxdl1")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "lcmb8twf",
+    "name": "favoris",
+    "type": "bool",
+    "required": false,
+    "unique": false,
+    "options": {}
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("2iqwzv3nrgzxdl1")
+
+  // remove
+  collection.schema.removeField("lcmb8twf")
+
+  return dao.saveCollection(collection)
+})
