abc5d060d89bc3a5647e73069f23db5054622876
Samantha-Saffache <samantha.saffache@edu.univ-fcomte.fr>
2023-06-06 11:36:42 +0200

    Co-authored-by: Romane Miguel <romanemgl@users.noreply.github.com>

---
+++ src/pages/publication.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+
+    <h1 class="font-bold text-center mb-[122px] lg:mb-[50px]">Que veux tu publier ?</h1>
+
+<div class="flex flex-col items-center gap-[44px]">
+    <RouterLink class="font-unbounded font-bold text-beige bg-orange py-[14px] px-[67px] rounded-[28px]" to="/recettes/create" >Une recette</RouterLink>
+    <RouterLink class="font-unbounded font-bold text-beige bg-orange py-[14px] px-[67px] rounded-[28px]" to="/produits/create" >Un produit</RouterLink>
+</div>
+
+</template>
